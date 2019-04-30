b4ec49d529e3da868430cb34eabab9e1cafdae55
shoheiyokoyama <shohei.yok0602@gmail.com>
2019-04-30 15:02:51 +0900

    Make project

---
+++ Chausie/Chausie.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for Chausie.
+FOUNDATION_EXPORT double ChausieVersionNumber;
+
+//! Project version string for Chausie.
+FOUNDATION_EXPORT const unsigned char ChausieVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Chausie/PublicHeader.h>
+
+
